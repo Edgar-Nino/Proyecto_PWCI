e961d374c8d64478a6a60eb9b9bf6673340042fd
Edgar-Nino <61064699+Edgar-Nino@users.noreply.github.com>
2020-11-29 23:50:50 -0600

    Se eliminaron las rutas categorias y listproducts

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -1,24 +1,27 @@
 const express = require('express');
 const cors = require('cors');
 const morgan = require('morgan');
-const bodyParser = require('body-parser')
+
+const cookieParser = require('cookie-parser')
 require('dotenv').config();
 
 const app = express();
 
 app.set('port',process.env.PORT||4000);
 
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(cors({origin:"*",exposedHeaders: "auth-token"}));
+
+app.use(cors({origin:["http://localhost:5500","http://127.0.0.1:5500/","*"],exposedHeaders: "auth-token",credentials:true}));
 app.use(morgan('dev'));
-app.use(express.json());
-app.use(express.urlencoded({extended:false}));
+app.use(cookieParser());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json())
 
 app.use(express.static(__dirname+'/public/'));
 
 app.use('/api/products',require('./routes/products.routes'));
 app.use('/api/users',require('./routes/users.routes'));
 app.use('/api/lists',require('./routes/lists.routes'));
-app.use('/api/categories',require('./routes/categories.routes'));
+// app.use('/api/categories',require('./routes/categories.routes'));
+// app.use('/api/listproducts',require('./routes/listProducts.routes'));
 
 module.exports = app;
