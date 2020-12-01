43c022f4607f05005711e398820e94504d862cb4
Edgar-Nino <61064699+Edgar-Nino@users.noreply.github.com>
2020-12-01 01:53:00 -0600

    Se agrego validacion y navegacion

--- Backend/src/controllers/users.controller.js
+++ Backend/src/controllers/users.controller.js
@@ -9,9 +9,10 @@ const jwt = require('jsonwebtoken');
 const fs = require('fs')
 
 
+
 usersCtrl.getUsers = async (req, res) => {
     try {
-        var users = await User.find({$and:[{pubpriv:true}]},{password:0})
+        var users = await User.find({ $and: [{ pubpriv: true }] }, { password: 0 })
 
         return res.json(users);
     }
@@ -20,12 +21,31 @@ usersCtrl.getUsers = async (req, res) => {
     }
 }
 
+usersCtrl.getUsersNav = async (req, res) => {
+    try {
+        var numberPage = (req.params.id) ? req.params.id : 1;
+        var numberDocs = 6 * numberPage;
+        var numberSkipped = 6 * (numberPage - 1)
+        const users = await User.find({ pubpriv: true }).skip(numberSkipped).limit(numberDocs).sort({ createdAt: -1 })
+        res.json(users);
+    }
+    catch (e) {
+        res.status(500).json({ status: 'No se pudieron conseguir los usuarios' })
+    }
+}
+
 usersCtrl.searchUser = async (req, res) => {
     try {
-        var string = req.params.id;
-        var regex = new RegExp([ string])
+        var numberPage = (req.params.page) ? req.params.page : 1;
+        var numberDocs = 6 * numberPage;
+        var numberSkipped = 6 * (numberPage - 1)
 
-        const users = await User.find({ $and: [{username:{ $regex: regex, $options: 'i' }}, { $or: [{ pubpriv: true }, { user_id: req.userId }] }] })
+        var string = req.params.id;
+        var regex = new RegExp([string])
+
+        const users = await User.find(
+            { $and: [{ username: { $regex: regex, $options: 'i' } }, { $or: [{ pubpriv: true }, { user_id: req.userId }] }] })
+            .skip(numberSkipped).limit(numberDocs).sort({ createdAt: -1 })
         console.log(users)
         res.json(users);
     }
@@ -36,8 +56,8 @@ usersCtrl.searchUser = async (req, res) => {
 }
 
 usersCtrl.getUser = async (req, res) => {
-    try {   
-        var users = await User.findOne({$and:[{pubpriv:true},{_id:req.params.id}]}, { password: 0 })
+    try {
+        var users = await User.findOne({ $and: [{ pubpriv: true }, { _id: req.params.id }] }, { password: 0 })
 
         return res.json(users);
     }
@@ -58,25 +78,32 @@ usersCtrl.profile = async (req, res) => {
 
 usersCtrl.editUser = async (req, res) => {
     try {
+        if(validateUser(req.body))return res.status(400).json({ status: 'El username tiene que tener minimo 3 caracteres, el email si tiene que ser un email y la contraseña debe de contener 8 o más caracteres, incluir 1 mayuscula, 1 minuscula, un caracter especial y un número' });
+
         var user = await User.findById(req.userId, { password: 0 })
 
-        if (!user) return res.status(400).json({ status: 'No has ingresado' })
+        if (!user) {
+            await fs.unlink('./src/public/uploads/' + req.file.filename, (err) => { })
+            return res.status(400).json({ status: 'No has ingresado' })
+        }
         const editUser = new User();
         req.body.password = await editUser.encryptPassword(req.body.password);
+
+        console.log(req.body)
 
         req.body.imgURL = req.file.filename;
 
         var oldUser = await User.findByIdAndUpdate(req.userId, req.body);
 
-        await fs.unlink('./src/public/uploads/'+ user.imgURL, (err)=>{console.log(err)})
+        await fs.unlink('./src/public/uploads/' + user.imgURL, (err) => { console.log(err) })
 
-        await List.updateMany({user_id:oldUser._id}, {username:req.body.username});
+        await List.updateMany({ user_id: oldUser._id }, { username: req.body.username });
 
         return res.json({ status: 'Se actualizo el usuario' });
     }
     catch (e) {
         console.log(e)
-        await fs.unlink('./src/public/uploads/'+ req.file.filename, (err)=>{})
+        await fs.unlink('./src/public/uploads/' + req.file.filename, (err) => { })
         return res.status(500).json({ status: 'No se actualizo el usuario' })
     }
 }
@@ -89,13 +116,13 @@ usersCtrl.deleteUser = async (req, res) => {
 
         var oldUser = await User.findByIdAndDelete(req.userId);
 
-        await List.deleteMany({user_id: oldUser._id})
+        await List.deleteMany({ user_id: oldUser._id })
 
-        await Product.deleteMany({user_id: oldUser.user_id})
+        await Product.deleteMany({ user_id: oldUser.user_id })
 
-        await fs.unlink('./src/public/uploads/'+ user.imgURL, (err)=>{console.log(err)})
+        await fs.unlink('./src/public/uploads/' + user.imgURL, (err) => { console.log(err) })
 
-        res.status(200).clearCookie("auth-token").json({status: "El usuario se ha eliminado con exito"});
+        res.status(200).clearCookie("auth-token").json({ status: "El usuario se ha eliminado con exito" });
 
     } catch (e) {
         return res.status(500).json({ status: 'No se pudo eliminar el usuario' })
@@ -104,11 +131,13 @@ usersCtrl.deleteUser = async (req, res) => {
 
 usersCtrl.logIn = async (req, res) => {
     try {
-        
-        const user = await User.findOne({$or: [
-            {email: req.body.identifier},
-            {username: req.body.identifier}
-        ]});
+
+        const user = await User.findOne({
+            $or: [
+                { email: req.body.identifier },
+                { username: req.body.identifier }
+            ]
+        });
 
         if (!user) return res.status(400).json({ status: 'El email o la contraseña estan mal' });
 
@@ -126,6 +155,12 @@ usersCtrl.logIn = async (req, res) => {
 
 usersCtrl.signUp = async (req, res) => {
     try {
+
+        if (validateUser(req.body)) {
+            await fs.unlink('./src/public/uploads/' + req.file.filename, (err) => { })
+            return res.status(400).json({ status: 'El username tiene que tener minimo 3 caracteres, el email si tiene que ser un email y la contraseña debe de contener 8 o más caracteres, incluir 1 mayuscula, 1 minuscula, un caracter especial y un número' });
+        }
+
         const newUser = new User(req.body);
         newUser.imgURL = req.file.filename;
         newUser.password = await newUser.encryptPassword(newUser.password);
@@ -135,18 +170,40 @@ usersCtrl.signUp = async (req, res) => {
     } catch (e) {
         console.log(e);
         res.status(500).json({ status: e.message })
-        await fs.unlink('./src/public/uploads/'+ req.file.filename, (err)=>{})
+        await fs.unlink('./src/public/uploads/' + req.file.filename, (err) => { })
     }
 
 }
 
 usersCtrl.logOut = async (req, res) => {
     try {
-        res.status(200).clearCookie("auth-token").json({status: "Se hizo el logout con exito"});
+        res.status(200).clearCookie("auth-token").json({ status: "Se hizo el logout con exito" });
     } catch (e) {
         res.status(500).json({ status: e.message })
     }
 
+}
+
+function validateUser(data) {
+    if (validate_length(data.username, 3)) return false;
+
+    if (validar_email(data.email, 3)) return false;
+
+    if (validate_password(data.password)) return false
+}
+
+function validar_email(email) {
+    var regex = /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+    return regex.test(email) ? true : false;
+}
+
+function validate_length(text, num) {
+    return (text.length >= num) ? true : false;
+}
+
+function validate_password(text) {
+    var REGEXpass = /^(?=.*[A-Z])(?=.*\d)(?=.*[!"#$%&/()=?¡¿])[A-Za-z\d!"#$%&/()=?¡¿]{8,}$/
+    return REGEXpass.test(text) ? true : false;
 }
 
 module.exports = usersCtrl;
