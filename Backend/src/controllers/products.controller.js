bb82de54e66dfce94745ef4ef80fb9f40b8d874b
Edgar-Nino <61064699+Edgar-Nino@users.noreply.github.com>
2020-12-01 01:52:33 -0600

    Cambios al controlador productos

--- Backend/src/controllers/products.controller.js
+++ Backend/src/controllers/products.controller.js
@@ -8,7 +8,7 @@ const fs = require('fs')
 
 productsCtrl.getProducts = async (req, res) => {
     try {
-        const products = await Product.find()
+        const products = await Product.find().sort({ createdAt: -1 })
         res.json(products);
     }
     catch (e) {
@@ -18,10 +18,10 @@ productsCtrl.getProducts = async (req, res) => {
 
 productsCtrl.getProductsNav = async (req, res) => {
     try {
-        var numberPage = (req.params.id)?req.params.id:1;
+        var numberPage = (req.params.id) ? req.params.id : 1;
         var numberDocs = 6 * numberPage;
-        var numberSkipped = 6 * ( numberPage-1)
-        const products = await Product.find().skip(numberSkipped).limit(numberDocs)
+        var numberSkipped = 6 * (numberPage - 1)
+        const products = await Product.find().skip(numberSkipped).limit(numberDocs).sort({ createdAt: -1 })
         res.json(products);
     }
     catch (e) {
@@ -32,13 +32,24 @@ productsCtrl.getProductsNav = async (req, res) => {
 productsCtrl.createProduct = async (req, res) => {
     try {
 
-        const user = await User.findOne({_id: req.userId},);
+        const user = await User.findOne({ _id: req.userId },);
 
-        if (!user) return res.status(400).json({ status: 'No tiene permitido crear productos' });
+        if (!user) {
+            await fs.unlink('./src/public/uploads/' + req.file.filename, (err) => { })
+            return res.status(400).json({ status: 'No tiene permitido crear productos' });
+        }
 
-        const list = await List.findOne({_id: req.body.id_list},);
+        const list = await List.findOne({ _id: req.body.id_list },);
 
-        if (!list) return res.status(400).json({ status: 'No existe la lista donde quieres poner tu producto' });
+        if (!list) {
+            await fs.unlink('./src/public/uploads/' + req.file.filename, (err) => { })
+            return res.status(400).json({ status: 'No existe la lista donde quieres poner tu producto' });
+        }
+
+        if (list.user_id != req.userId) {
+            await fs.unlink('./src/public/uploads/' + req.file.filename, (err) => { })
+            return res.status(400).json({ status: 'No eres dueño de la lista' });
+        }
 
         const newProduct = new Product(req.body);
 
@@ -52,7 +63,7 @@ productsCtrl.createProduct = async (req, res) => {
     catch (e) {
         console.log(e);
         res.status(500).json({ status: "No se pudo crear el producto" })
-        await fs.unlink('./src/public/uploads/'+ req.file.filename, (err)=>{})
+        await fs.unlink('./src/public/uploads/' + req.file.filename, (err) => { })
     }
 }
 
@@ -70,12 +81,19 @@ productsCtrl.editProduct = async (req, res) => {
     try {
         const product = await Product.findOne({ _id: req.params.id });
 
-        if(!product)return res.status(400).json({ status: 'No se encontro el producto a actualizar' });
+        if (!product) {
+            await fs.unlink('./src/public/uploads/' + req.file.filename, (err) => { })
+            return res.status(400).json({ status: 'No se encontro el producto a actualizar' });
+        }
 
-        const list = await List.findOne({ _id: product.id_list});
+        const list = await List.findOne({ _id: product.id_list });
 
-        const user = await User.findOne({_id: req.userId},);
-        if (!user||user._id!=list.user_id) return res.status(400).json({ status: 'No tiene permitido actualizar productos' });
+        const user = await User.findOne({ _id: req.userId },);
+
+        if (!user || user._id != list.user_id) {
+            await fs.unlink('./src/public/uploads/' + req.file.filename, (err) => { })
+            return res.status(400).json({ status: 'No tiene permitido actualizar productos' });
+        }
 
         req.body.user_id = user._id;
         req.body.id_list = product.id_list;
@@ -84,24 +102,24 @@ productsCtrl.editProduct = async (req, res) => {
 
         await Product.findByIdAndUpdate(req.params.id, req.body);
 
-        await fs.unlink('./src/public/uploads/'+ product.imgURL, (err)=>{console.log(err)})
+        await fs.unlink('./src/public/uploads/' + product.imgURL, (err) => { console.log(err) })
 
         res.json({ status: 'Se actualizo el producto' });
     }
     catch (e) {
         res.status(500).json({ status: 'No se actualizo el producto' })
-        await fs.unlink('./src/public/uploads/'+ req.file.filename, (err)=>{})
+        await fs.unlink('./src/public/uploads/' + req.file.filename, (err) => { })
     }
 }
 
 productsCtrl.deleteProduct = async (req, res) => {
     try {
-        const user = await User.findOne({_id: req.userId},);
+        const user = await User.findOne({ _id: req.userId },);
         if (!user) return res.status(400).json({ status: 'No tiene permitido borrar productos' });
 
         var product = await Product.findByIdAndDelete(req.params.id);
 
-        await fs.unlink('./src/public/uploads/'+ product.imgURL, (err)=>{console.log(err)})
+        await fs.unlink('./src/public/uploads/' + product.imgURL, (err) => { console.log(err) })
 
         res.json({ status: 'Se borro el producto' })
     }
